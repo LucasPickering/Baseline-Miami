9881922403cf1dadce2c62860f8f967b368c7d67
Lucas Pickering <pickering.l@husky.neu.edu>
2016-03-03 15:23:30 -0500

    Added player movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+
+	private CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float horizSpeed = speed * Input.GetAxis("Horizontal");
+		float vertSpeed = speed * Input.GetAxis("Vertical");
+		controller.Move(new Vector3(horizSpeed, vertSpeed, 0f));
+	}
+}
