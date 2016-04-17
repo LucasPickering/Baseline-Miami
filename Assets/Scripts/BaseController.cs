8b2ffb9ca66f8ddf05ee6206a3ed0b527925bb35
Lucas Pickering <pickering.l@husky.neu.edu>
2016-04-17 12:34:35 -0400

    Apprently this was missing?

---
+++ Assets/Scripts/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseController : MonoBehaviour {
+
+	void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log("Potato");
+    }
+}
