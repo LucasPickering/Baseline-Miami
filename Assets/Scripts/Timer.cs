eb594635ef0bc72a0de091b1c03b8def20d60ef9
chec <cmche0221@gmail.com>
2016-04-28 13:56:14 -0400

    Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	private float timer = 0.0f;
+	private GameObject[] opponentsAlive;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		opponentsAlive = GameObject.FindGameObjectsWithTag("Opponent");
+
+		if (opponentsAlive.Length == 0) {
+			Time.timeScale = 0;
+		} else {
+			timer += Time.deltaTime;
+			gameObject.GetComponent<Text> ().text = timer.ToString ();
+		}
+	}
+}
