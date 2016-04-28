c034efece5215fd03545c570d5557e5c515df2c1
chec <cmche0221@gmail.com>
2016-04-27 20:08:44 -0400

    Missing Opponent Scripts

---
+++ Assets/Scripts/OpponentAI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpponentAI : MonoBehaviour {
+
+	public GameObject target;
+	public float speed = 2f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pos.z = -1;
+		transform.position = pos;
+
+		Vector3 targetHeading = target.transform.position - transform.position;
+		Vector3 targetDirection = targetHeading.normalized;
+
+		// Move towards the player
+		if (Vector3.Distance(transform.position,target.transform.position) < 3f){//move if distance from target is greater than 1
+			transform.position += targetDirection * speed * Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/Scripts/OpponentBallCollision.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpponentBallCollision : MonoBehaviour {
+
+	private GameObject[] ball;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		ball = GameObject.FindGameObjectsWithTag("Ball");
+
+		foreach (GameObject b in ball) {
+			if (Vector3.Distance (transform.position, b.transform.position) < 1f) {
+				gameObject.active = false;
+			}
+		}
+
+	}
+	/*
+	void OnCollisionEnter2D(Collision2D target) {
+		if (target.gameObject.name == "Ball(Clone)") {
+			gameObject.active = false;
+		}
+	}
+	*/
+}
