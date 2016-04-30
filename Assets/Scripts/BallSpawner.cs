427b09e794ddc385427968b52896658b52ccaeff
Lucas Pickering <pickering.l@husky.neu.edu>
2016-04-29 20:54:13 -0400

    God you have to try this

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -9,13 +9,25 @@ public class BallSpawner : MonoBehaviour
 	public float minSpeed;
 	public float maxSpeed;
 	public float lifeTime;
+	public float spawnInterval;
 
 	void Update ()
 	{
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			GameObject ball = (GameObject)Instantiate (ballPrefab, ballPrefab.transform.position, Quaternion.identity);
-			ball.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0f, -Random.Range (minSpeed, maxSpeed)); // Random start speed in range
-			Destroy(ball, lifeTime); // Kill the ball after a certain amount of time
+		StartCoroutine ("SpawnBalls");
+	}
+
+	IEnumerator SpawnBalls()
+	{
+		while (true) {
+			SpawnBall ();
+			yield return new WaitForSeconds(spawnInterval);
 		}
+	}
+
+	private void SpawnBall()
+	{
+		GameObject ball = (GameObject)Instantiate (ballPrefab, ballPrefab.transform.position, Quaternion.identity); // Spawn the ball
+		ball.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0f, -Random.Range (minSpeed, maxSpeed)); // Random start speed in range
+		Destroy(ball, lifeTime); // Kill the ball after a certain amount of time
 	}
 }
